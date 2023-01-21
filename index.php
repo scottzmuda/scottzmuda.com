6f5350b71701ada9d64edfb4e9bb907c9a79689c
scottzmuda <109922711+scottzmuda@users.noreply.github.com>
2023-01-21 12:53:27 -0600

    Create index.php
    adding index.php so site is detected as php website

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "db-mysql-scott-do-user-12091905-0.b.db.ondigitalocean.com";
+$username = "scottzmuda";
+$password = "AVNS_wNEmYo_o8obEIcD69Km";
+$dbname = "website";
+$port = "25060"
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
