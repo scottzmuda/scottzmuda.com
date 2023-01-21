04125d45c82548e1394bd02513f009a6f1ca1fce
scottzmuda <109922711+scottzmuda@users.noreply.github.com>
2023-01-21 16:24:05 -0600

    referencing environmental variables
    probably safer

--- connect.php
+++ connect.php
@@ -1,8 +1,2 @@
 <?php
-$servername = "db-mysql-scott-do-user-12091905-0.b.db.ondigitalocean.com";
-$username = "scottzmuda";
-$password = "AVNS_wNEmYo_o8obEIcD69Km";
-$dbname = "website";
-$port = "25060"
-
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
