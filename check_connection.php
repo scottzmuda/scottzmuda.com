ce8910143c7228d911db655fa17a0b5db065055b
scottzmuda <109922711+scottzmuda@users.noreply.github.com>
2023-01-20 23:02:53 -0600

    Update check_connection.php
    trying...

--- check_connection.php
+++ check_connection.php
@@ -1,19 +1,4 @@
 <?php
     $client_ip = $_SERVER['REMOTE_ADDR'];
-    echo "The client's IP address is: " . $client_ip;
-    
-$servername = "db-mysql-scott-do-user-12091905-0.b.db.ondigitalocean.com";
-$username = "scottzmuda";
-$password = "AVNS_wNEmYo_o8obEIcD69Km";
-$dbname = "website";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connection Successful";
-    $conn->close();
+    echo "The client's IP address is: ".$client_ip;
 ?>
