1ff9cdd2c421dd769454ab643e0d1f073d3db9b5
scottzmuda <109922711+scottzmuda@users.noreply.github.com>
2023-01-20 21:03:06 -0600

    second attempt at post form
    added php to separate file.

---
+++ submit.php
@@ -0,0 +1,41 @@
+<?php
+//Validate the form data
+if(empty($_POST['title']) || empty($_POST['description']) || empty($_POST['writing'])){
+    echo "Please fill all the fields";
+    exit;
+}
+
+//Connect to the database
+$servername = "db-mysql-scott-do-user-12091905-0.b.db.ondigitalocean.com";
+$username = "scottzmuda";
+$password = "AVNS_wNEmYo_o8obEIcD69Km";
+$dbname = "website";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//Get the form data
+$title = $_POST['title'];
+$description = $_POST['description'];
+$writing = $_POST['writing'];
+
+//Insert data into the table
+$sql = "INSERT INTO creations (title, description, writing) VALUES ($title, $description, $writing)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $title, $description, $writing);
+$stmt->execute();
+
+//Check if data was inserted successfully
+if($stmt->affected_rows === 1){
+    echo "Data inserted successfully";
+}else{
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
