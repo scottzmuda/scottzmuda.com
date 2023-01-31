b072beaa81d523ee42a129d8966fc032383a1db9
scottzmuda <109922711+scottzmuda@users.noreply.github.com>
2023-01-30 20:41:05 -0600

    adding /index and '/' machinery
    (also renamed /writings/ -> /w/)

---
+++ flask_app/controllers/index_controller.py
@@ -0,0 +1,7 @@
+from flask_app import app
+from flask import render_template, request, redirect
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
--- flask_app/controllers/writing_controller.py
+++ flask_app/controllers/writing_controller.py
@@ -2,11 +2,11 @@ from flask_app import app
 from flask import render_template, request, redirect
 from flask_app.models import writing
 
-@app.route('/writings/writing-form')
+@app.route('/w/add-writing')
 def draft_new_writing():
-    return render_template("new-writing-form.html")
+    return render_template("add-writing.html")
 
-@app.route('/writings/save-new-writing', methods=["POST"])
+@app.route('/w/save-writing', methods=["POST"])
 def save_new_writing():
     data = {
         "title": request.form["title"],
@@ -15,4 +15,4 @@ def save_new_writing():
     }
 
     writing.Writing.save( data )
-    return redirect('/writings/writing-form')
+    return redirect('/w/add-writing')
