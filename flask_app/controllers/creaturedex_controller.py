7fae9851b6262464c1231230581ec6f6e9a08289
czmud <caleb.zmuda@protonmail.com>
2023-02-03 18:27:18 -0800

    Add creaturedex connection to DB and model

--- flask_app/controllers/creaturedex_controller.py
+++ flask_app/controllers/creaturedex_controller.py
@@ -1,7 +1,9 @@
 from flask_app import app
 from flask import render_template, request, redirect
+from flask_app.models import creature
 
 @app.route('/c')
 @app.route('/c/')
 def creatures():
-    return render_template("creaturedex.html")
+    creatures = creature.Creature.get_all()
+    return render_template("creaturedex.html", creatures=creatures)
