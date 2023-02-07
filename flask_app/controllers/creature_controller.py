78bae48527adc4c8679b713e6643e6ccd922ecde
scottzmuda <109922711+scottzmuda@users.noreply.github.com>
2023-02-06 21:35:49 -0600

    correction to new creature url
    whoopsie

--- flask_app/controllers/creature_controller.py
+++ flask_app/controllers/creature_controller.py
@@ -2,10 +2,10 @@ from flask_app import app
 from flask import render_template
 from flask_app.models import creature
 
-@app.route('/c/<int:creature_id>')
-def view_creature( creature_id ):
-    one_creature = creature.Creature.get_creature_by_id( {"id": creature_id} )
-    return render_template("view-creature.html", creature=one_creature )
+#@app.route('/c/<int:creature_id>')
+#def view_creature( creature_id ):
+#    one_creature = creature.Creature.get_creature_by_id( {"id": creature_id} )
+#    return render_template("view-creature.html", creature=one_creature )
 
 @app.route('/c/<int:creature_time_s>-<string:creature_name>')
 def view_creature2( creature_time_s ):
