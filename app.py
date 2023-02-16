8a3443d3d9a181562bf6bc16385c40bfdf04426f
czmud <caleb.zmuda@protonmail.com>
2023-02-16 13:35:21 -0800

    Update controller import to multi-line

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask_app.controllers import writingdex_controller, creaturedex_controller, index_controller, writing_controller, creature_controller
+from flask_app.controllers import (
+    writingdex_controller,
+    creaturedex_controller,
+    index_controller,
+    writing_controller, 
+    creature_controller,
+    error_controller,
+)
 from flask_app import app
 
 if __name__ == "__main__":
