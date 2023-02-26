02eb8dc3b1c0e9f0b48211d4208fc15a54498576
czmud <caleb.zmuda@protonmail.com>
2023-02-25 17:41:11 -0800

    update controller

--- flask_app/controllers/living_thing_controller.py
+++ flask_app/controllers/living_thing_controller.py
@@ -1,6 +1,7 @@
 from flask_app import app
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, flash,request, session
 from flask_app.models import living_thing
+from flask_app.utilities import time_util
 
 @app.route('/l')
 @app.route('/l/')
@@ -48,14 +49,57 @@ def create_living_thing():
 def save_living_thing():
     if 'creator_id' not in session:
         return redirect('/l')
+    
+    print(request.form)
+
+    if not living_thing.Living_thing.validate_living_thing_form(request.form):
+        session['name'] = request.form['name']
+        session['name_scientific'] = request.form['name_scientific']
+        session['description'] = request.form['description']
+        session['lat_deg'] = request.form['lat_deg']
+        session['long_deg'] = request.form['long_deg']
+        session['elev_m'] = request.form['elev_m']
+        session['date_time'] = request.form['date_time']
+        return redirect('/l/create-living_thing')
+
+    check_date_time = time_util.date_time_offset_to_utc_sec(request.form['date_time'], request.form['utc_offset_minutes'])
+    
+    # loop for 100 tries, else flash message to user to choose new time
+    count = 0
+    while count < 3:
+        # query to confirm we do not already have an entry for same utc time
+        if not living_thing.Living_thing.get_living_thing_by_time({'time_s': check_date_time}):
+            break
+        check_date_time += 1
+        count += 1
+    
+    if count >= 3:
+        flash('database is full for that timestamp, please select a different timestamp for your post', "create_living_thing")
+        session['name'] = request.form['name']
+        session['name_scientific'] = request.form['name_scientific']
+        session['description'] = request.form['description']
+        session['lat_deg'] = request.form['lat_deg']
+        session['long_deg'] = request.form['long_deg']
+        session['elev_m'] = request.form['elev_m']
+        session['date_time'] = request.form['date_time']
+        return redirect('/l/create-living_thing')
+
     data = {
-        "name": request.form["name"],
-        "name_scientific": request.form["name_scientific"],
-        "description": request.form["description"],
-        "lat_deg": request.form["lat_deg"],
-        "long_deg": request.form["long_deg"],
-        "elev_m": request.form["elev_m"],
-        "time_s": request.form["time_s"]
+        "creator_id": session['creator_id'],
+        "name": request.form['name'],
+        "name_scientific": request.form['name_scientific'],
+        "description": request.form['description'],
+        "lat_deg": request.form['lat_deg'],
+        "long_deg": request.form['long_deg'],
+        "elev_m": request.form['elev_m'],
+        "time_s": check_date_time
     }
-    living_thing.Living_thing.save( data )
-    return redirect('/l/create-living_thing')
+
+    living_thing_id = living_thing.Living_thing.save( data )
+    one_living_thing = living_thing.Living_thing.get_living_thing_by_id( {"id": living_thing_id} )
+
+    hold_creator_id = session['creator_id']
+    session.clear()
+    session['creator_id'] = hold_creator_id
+
+    return redirect('/l/'+one_living_thing.url_string)
